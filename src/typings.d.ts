2877123dff7478181e201fd6a0f3d64b522ac288
melany-pedemonte <mpedemonte@qwavee.com>
2023-03-22 16:11:31 -0300

    init app

--- src/typings.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "stylis-plugin-rtl" {
-  const noTypesYet: any;
-  export default noTypesYet;
-}
-
-declare module "jsvectormap";
-declare module "jsvectormap/dist/maps/world-merc";
-declare module "jsvectormap/dist/maps/world";
